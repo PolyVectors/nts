3f88a3dbcabf8a7369b0d8199f0df974fba34b6b
PolyVectors <PolyVector@protonmail.com>
2025-06-10 00:42:45 +0100

    suggestions

--- src/main.c
+++ src/main.c
@@ -5,7 +5,16 @@ void enableRawMode() {
   struct termios raw;
 
   termios_getattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(TERMIOS_ECHO);
+
+  /* TODO (https://github.com/EastonA01/text-editor-in-c/blob/master/kilo.c#L146-L151):
+  
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+  */
 
   termios_setattr(STDIN_FILENO, TERMIOS_SETSF, &raw);
 }
