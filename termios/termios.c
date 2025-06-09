eba13cf93492c2ade2dd4adbd2506f7a072d962c
PolyVectors <PolyVector@protonmail.com>
2025-06-09 01:43:38 +0100

    can't rename kernel structure members

--- termios/termios.c
+++ termios/termios.c
@@ -1,16 +1,26 @@
 #include "../std/std.h"
 #include "termios.h"
 
-i32 termios_getattr(i32 fd, struct termios *termios_p) {
-  struct termios k_termios;
-
+i32 termios_getattr(i32 fd, struct termios* termios_p) {
+  struct termios copy_termios;
   // https://linux.die.net/man/2/ioctl_list
-  i32 retval = std_sys_syscall3(16, fd, 0x00005401, (u64)&k_termios);
+  int ret_value = std_sys_syscall3(16, fd, 0x00005401, (u64)&copy_termios);
 
-  if (retval == 0)
-    std_io_print(STDOUT_FILENO, "we're in\n");
+  if (ret_value == 0) {
+    termios_p->c_iflag = copy_termios.c_iflag;
+    termios_p->c_oflag = copy_termios.c_oflag;
+    termios_p->c_iflag = copy_termios.c_cflag;
+    termios_p->c_lflag = copy_termios.c_lflag;
 
-  return retval;
+
+    // todo: control characters
+    termios_p->c_line = copy_termios.c_line;
+
+    termios_p->c_ispeed = copy_termios.c_ispeed;
+    termios_p->c_ospeed = copy_termios.c_ospeed;
+  } 
+
+  return ret_value;
 }
 
 i32 termios_setattr(i32 fd, i32 optional_actions, const struct termios *termios_p) {
--- termios/termios.h
+++ termios/termios.h
@@ -8,17 +8,17 @@
 // this is actually __kernel_termios, no abstractions
 
 struct termios {
-  u32 iflag;
-  u32 oflag;
-  u32 cflag;
-  u32 lflag;
+  u32 c_iflag;
+  u32 c_oflag;
+  u32 c_cflag;
+  u32 c_lflag;
   
-  u8 cchars[19];
-  u8 line;
+  u8 c_cc[19];
+  u8 c_line;
   
-  i32 ispeed;
-  i32 ospeed;
+  i32 c_ispeed;
+  i32 c_ospeed;
 };
 
-i32 termios_getattr(i32 fd, struct termios *termios_p);
+i32 termios_getattr(i32 fd, struct termios* termios_p);
 i32 termios_setattr(i32 fd, i32 optional_actions, const struct termios *termios_p);
